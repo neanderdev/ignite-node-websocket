74e3c1bd867ab52c8db446f106b503183874b936
neanderdev <neander.faria@hotmail.com>
2022-07-07 09:45:14 -0300

    Configuring database

--- src/http.ts
+++ src/http.ts
@@ -2,10 +2,16 @@ import express from "express";
 import path from "path";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import mongoose, { ConnectOptions } from "mongoose";
 
 const app = express();
 
 const server = createServer(app);
+
+mongoose.connect("mongodb://localhost/rocketsocket", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+} as ConnectOptions);
 
 app.use(express.static(path.join(__dirname, "..", "public")));
 
