103b169150214bffe284edf6c0c7d6709cc79c36
neanderdev <neander.faria@hotmail.com>
2022-07-07 10:44:19 -0300

    Creating user communication room

--- src/websocket/ChatService.ts
+++ src/websocket/ChatService.ts
@@ -6,6 +6,7 @@ import { CreateUserService } from "../services/CreateUserService";
 import { GetAllUserService } from "../services/GetAllUserService";
 import { CreateChatRoomService } from "../services/CreateChatRoomService";
 import { GetUserBySocketIdService } from "../services/GetUserBySocketIdService";
+import { GetChatRoomByUsersService } from "../services/GetChatRoomByUsersService";
 
 io.on("connect", (socket) => {
   socket.on("start", async (data) => {
@@ -36,13 +37,20 @@ io.on("connect", (socket) => {
     const getUserBySocketIdService = container.resolve(
       GetUserBySocketIdService
     );
+    const getChatRoomByUsersService = container.resolve(
+      GetChatRoomByUsersService
+    );
 
     const userLogged = await getUserBySocketIdService.execute(socket.id);
 
-    const room = await createChatRoomService.execute([
+    let room = await getChatRoomByUsersService.execute([
       data.idUser,
       userLogged._id,
     ]);
+
+    if (!room) {
+      room = await createChatRoomService.execute([data.idUser, userLogged._id]);
+    }
 
     callback(room);
   });
